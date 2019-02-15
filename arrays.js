08221154c683fc160d0aca12c162c491cd3104ff
Rashad Walcott <rashad.walcott15@gmail.com>
2019-02-15 22:45:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 function addElementToEndOfArray(array,element){
   //should not alter the original array
- [...array,element];
+array= [...array,element];
   return array;
 }
 
