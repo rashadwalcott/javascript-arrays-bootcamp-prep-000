6fe6d3269f71bbe716ffdcda2ef4cd35aeabb1f8
Rashad Walcott <rashad.walcott15@gmail.com>
2019-02-15 23:37:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array=array.shift();
+  array.shift();
   return array;
 }
 function removeElementFromBeginningOfArray(array){
